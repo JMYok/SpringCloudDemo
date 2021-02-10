49fcc30ecae9aa83b1fc88d74410d5a9c70aaa2f
BobJiang <374123140@qq.com>
2021-02-11 05:44:57 +0800

    provider集群访问

--- pro08-spring-cloud-provider/src/main/java/com/atguigu/spring/cloud/handler/EmployeeHandler.java
+++ pro08-spring-cloud-provider/src/main/java/com/atguigu/spring/cloud/handler/EmployeeHandler.java
@@ -1,5 +1,7 @@
 package com.atguigu.spring.cloud.handler;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,13 @@ import com.atguigu.cloud.entity.Employee;
 
 @RestController
 public class EmployeeHandler {
-	
+
 	@RequestMapping("/provider/get/employee/remote")
-	public Employee getEmployeeRemote() {
-		return new Employee(555, "tom555", 555.55);
+	public Employee getEmployeeRemote(HttpServletRequest request) {
+
+		// 获取当前 Web 应用的端口号 
+		int serverPort = request.getServerPort();
+
+		return new Employee(555, "tom555-"+serverPort, 555.55);
 	}
 }
--- pro09-spring-cloud-consumer/src/main/java/com/atguigu/spring/cloud/handler/HumanResourceHandler.java
+++ pro09-spring-cloud-consumer/src/main/java/com/atguigu/spring/cloud/handler/HumanResourceHandler.java
@@ -12,11 +12,13 @@ public class HumanResourceHandler {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@RequestMapping("/consumer/get/employee")
+	@RequestMapping("/consumer/ribbon/get/employee")
 	public Employee getEmployeeRemote() {
 
 		// 远程调用方法的主机地址
-		String host = "http://localhost:1000";
+		//String host = "http://localhost:1000";
+		//引入Eureka和Ribbon后，就可以使用微服务名称替代IP地址+端口号
+		String host = "http://atguigu-provider";	
 
 		// 远程调用方法的具体 URL 地址 ,以及返回类型
 		String url = "/provider/get/employee/remote";
