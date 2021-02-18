fb71fbd7537c4b8e3aae12c32640f4c01358f02d
BobJiang <374123140@qq.com>
2021-02-19 07:34:19 +0800

    Hystyix降级

--- pro07-spring-cloud-common/src/main/java/com/atguigu/spring/cloud/api/EmployeeRemoteService.java
+++ pro07-spring-cloud-common/src/main/java/com/atguigu/spring/cloud/api/EmployeeRemoteService.java
@@ -2,12 +2,18 @@ package com.atguigu.spring.cloud.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.atguigu.spring.cloud.entity.Employee;
+import com.atguigu.spring.cloud.factory.MyFallBackFactory;
+import com.atguigu.spring.cloud.util.ResultEntity;
 
-@FeignClient("atguigu-provider") //表明当前接口和一个Provider对应
+@FeignClient(value = "atguigu-provider",fallbackFactory=MyFallBackFactory.class) //表明当前接口和一个Provider对应
 public interface EmployeeRemoteService {
 	
 	@RequestMapping("/provider/get/employee/remote")
 	public Employee getEmployeeRemote();
+	
+	@RequestMapping("/provider/get/emp/with/circuit/breaker")
+	public ResultEntity<Employee> getEmpWithCircuitBreaker(@RequestParam("signal") String signal);
 }
---
+++ pro07-spring-cloud-common/src/main/java/com/atguigu/spring/cloud/factory/MyFallBackFactory.java
@@ -0,0 +1,35 @@
+package com.atguigu.spring.cloud.factory;
+
+import org.springframework.stereotype.Component;
+
+import com.atguigu.spring.cloud.api.EmployeeRemoteService;
+import com.atguigu.spring.cloud.entity.Employee;
+import com.atguigu.spring.cloud.util.ResultEntity;
+
+import feign.hystrix.FallbackFactory;
+
+/**
+ * 实现consumer端服务降级功能
+ * 实现MyFallBackFactory端口时要传入@FeignClient注解标记的接口类型
+ * 当provider调用失败后会执行这个对象对应的方法
+ * 必须要有Component注解将其注入到IOC容器中
+ * @author dell
+ *
+ */
+@Component
+public class MyFallBackFactory implements FallbackFactory<EmployeeRemoteService>{
+
+	public EmployeeRemoteService create(final Throwable cause) {
+		return new EmployeeRemoteService() {
+			
+			public Employee getEmployeeRemote() {
+				return null;
+			}
+			
+			public ResultEntity<Employee> getEmpWithCircuitBreaker(String signal) {
+				return ResultEntity.failed("降级机制生效"+cause.getMessage());
+			}
+		};
+	}
+
+}
